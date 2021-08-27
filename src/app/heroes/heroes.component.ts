43dc5f6982be231878eceac7864242432ca77be3
AG <artemgolovin@AGs-MacBook-Air.local>
2021-08-27 19:10:14 +0300

    Figured out how to work with router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,22 +9,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  heroes: Hero[] = []
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
-  }
-
-  selectedHero?: Hero
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-  }
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
-  ngOnInit(): void {
-    this.getHeroes()
+  ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
